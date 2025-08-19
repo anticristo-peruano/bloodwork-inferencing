6be438f781f3e6107518948d9b92f273fd1ca930
sofos <isoschronos@gmail.com>
2025-08-19 16:02:21 -0500

    Typo

--- kb/snomed/pgsql_index.sql
+++ kb/snomed/pgsql_index.sql
@@ -1,22 +1,22 @@
 set schema 'snomedct';
 
-CREATE INDEX description_conceptid_idx ON snomedct.description_f
+CREATE INDEX description_conceptid_idx ON snomedct.description
   USING btree (conceptid COLLATE pg_catalog."default");
-CREATE INDEX textdefinition_conceptid_idx ON snomedct.textdefinition_f
+CREATE INDEX textdefinition_conceptid_idx ON snomedct.textdefinition
   USING btree (conceptid);
-CREATE INDEX relationship_f_idx ON snomedct.relationship_f
+CREATE INDEX relationship_f_idx ON snomedct.relationship
   USING btree (sourceid, destinationid);
-CREATE INDEX stated_relationship_f_idx ON snomedct.stated_relationship_f
+CREATE INDEX stated_relationship_f_idx ON snomedct.stated_relationship
   USING btree (sourceid, destinationid);
-CREATE INDEX langrefset_referencedcomponentid_idx ON snomedct.langrefset_f
+CREATE INDEX langrefset_referencedcomponentid_idx ON snomedct.langrefset
   USING btree (referencedcomponentid);
-CREATE INDEX associationrefset_f_idx ON snomedct.associationrefset_f
+CREATE INDEX associationrefset_f_idx ON snomedct.associationrefset
   USING btree (referencedcomponentid, targetcomponentid);
-CREATE INDEX attributevaluerefset_f_idx ON snomedct.attributevaluerefset_f
+CREATE INDEX attributevaluerefset_f_idx ON snomedct.attributevaluerefset
   USING btree (referencedcomponentid, valueid);
-CREATE INDEX simplerefset_referencedcomponentid_idx ON snomedct.simplerefset_f
+CREATE INDEX simplerefset_referencedcomponentid_idx ON snomedct.simplerefset
   USING btree (referencedcomponentid);
-CREATE INDEX extendedmaprefset_referencedcomponentid_idx ON snomedct.extendedmaprefset_f
+CREATE INDEX extendedmaprefset_referencedcomponentid_idx ON snomedct.extendedmaprefset
   USING btree (referencedcomponentid);
 
 CHECKPOINT;
