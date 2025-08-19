5178ba92b8308b5482ff611310835a478f390b4c
sofos <isoschronos@gmail.com>
2025-08-19 17:02:43 -0500

    Appropiately handled the datatypes

--- kb/umls/pgsql_tables.sql
+++ kb/umls/pgsql_tables.sql
@@ -2,33 +2,34 @@ drop schema if exists umls cascade;
 create schema umls;
 set schema 'umls';
 
+
 CREATE TABLE MRCOLS (
 	COL			varchar(20),
 	DES			varchar(200),
-	REF			varchar(20),
+	REF			varchar(40),
 	MIN			int,
 	AV			numeric(5,2),
 	MAX			int,
 	FIL			varchar(50),
-	DTY			varchar(20),
+	DTY			varchar(40),
 	dummy 		char(1)
 );
 
 
 CREATE TABLE  MRCONSO (
-	CUI			char(12) NOT NULL,
+	CUI			char(8) NOT NULL,
 	LAT			char(3) NOT NULL,
 	TS			char(1) NOT NULL,
-	LUI			char(12) NOT NULL,
+	LUI			char(10) NOT NULL,
 	STT			varchar(3) NOT NULL,
-	SUI			char(12) NOT NULL,
+	SUI			char(10) NOT NULL,
 	ISPREF		char(1) NOT NULL,
 	AUI			varchar(9) NOT NULL,
-	SAUI		varchar(100),
+	SAUI		varchar(50),
 	SCUI		varchar(100),
 	SDUI		varchar(100),
-	SAB			varchar(20) NOT NULL,
-	TTY			varchar(20) NOT NULL,
+	SAB			varchar(40) NOT NULL,
+	TTY			varchar(40) NOT NULL,
 	CODE		varchar(100) NOT NULL,
 	STR			text NOT NULL,
 	SRL			varchar(10) NOT NULL,
@@ -39,7 +40,7 @@ CREATE TABLE  MRCONSO (
 
 
 CREATE TABLE MRCUI (
-	CUI1		char(12) NOT NULL,
+	CUI1		char(8) NOT NULL,
 	VER			varchar(10) NOT NULL,
 	REL			varchar(4) NOT NULL,
 	RELA		varchar(100),
@@ -51,11 +52,11 @@ CREATE TABLE MRCUI (
 
 
 CREATE TABLE MRDEF (
-	CUI			char(12) NOT NULL,
+	CUI			char(8) NOT NULL,
 	AUI			varchar(9) NOT NULL,
-	ATUI		varchar(15) NOT NULL,
+	ATUI		varchar(11) NOT NULL,
 	SATUI		varchar(50),
-	SAB			varchar(20) NOT NULL,
+	SAB			varchar(40) NOT NULL,
 	DEF			text NOT NULL,
 	SUPPRESS	char(1) NOT NULL,
 	CVF			int,
@@ -84,24 +85,24 @@ CREATE TABLE MRFILES (
 
 
 CREATE TABLE MRHIER (
-	CUI			char(12) NOT NULL,
+	CUI			char(8) NOT NULL,
 	AUI			varchar(9) NOT NULL,
 	CXN			int NOT NULL,
-	PAUI		varchar(9),
-	SAB			varchar(20) NOT NULL,
+	PAUI		varchar(10),
+	SAB			varchar(40) NOT NULL,
 	RELA		varchar(100),
 	PTR			text,
-	HCD			varchar(70),
+	HCD			varchar(100),
 	CVF			int,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE MRHIST (
-	CUI			char(12) NOT NULL,
-	SOURCEUI	varchar(50) NOT NULL,
-	SAB			varchar(20) NOT NULL,
-	SVER		varchar(20) NOT NULL,
+	CUI			char(8) NOT NULL,
+	SOURCEUI	varchar(100) NOT NULL,
+	SAB			varchar(40) NOT NULL,
+	SVER		varchar(40) NOT NULL,
 	CHANGETYPE	text NOT NULL,
 	CHANGEKEY	text NOT NULL,
 	CHANGEVAL	text NOT NULL,
@@ -112,8 +113,8 @@ CREATE TABLE MRHIST (
 
 
 CREATE TABLE MRMAP (
-	MAPSETCUI	char(12),
-	MAPSETSAB	varchar(20),
+	MAPSETCUI	char(8),
+	MAPSETSAB	varchar(40),
 	MAPSUBSETID	varchar(10),
 	MAPRANK		int,
 	MAPID		varchar(50),
@@ -135,7 +136,7 @@ CREATE TABLE MRMAP (
 	MAPRULE		text,
 	MAPRES		text,
 	MAPTYPE		varchar(50),
-	MAPATN		varchar(20),
+	MAPATN		varchar(100),
 	MAPATV		text,
 	CVF			int,
 	dummy 		char(1)
@@ -144,26 +145,26 @@ CREATE TABLE MRMAP (
 
 CREATE TABLE MRRANK (
 	RANK		int NOT NULL,
-	SAB			varchar(20) NOT NULL,
-	TTY			varchar(20) NOT NULL,
+	SAB			varchar(40) NOT NULL,
+	TTY			varchar(40) NOT NULL,
 	SUPPRESS	char(1) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE MRREL (
-	CUI1		char(12) NOT NULL,
+	CUI1		char(8) NOT NULL,
 	AUI1		varchar(9),
 	STYPE1		varchar(50) NOT NULL,
 	REL			varchar(4) NOT NULL,
-	CUI2		char(12) NOT NULL,
+	CUI2		char(8) NOT NULL,
 	AUI2		varchar(9),
 	STYPE2		varchar(50) NOT NULL,
 	RELA		varchar(100),
 	RUI			varchar(10) NOT NULL,
 	SRUI		varchar(50),
-	SAB			varchar(20) NOT NULL,
-	SL			varchar(20) NOT NULL,
+	SAB			varchar(40) NOT NULL,
+	SL			varchar(40) NOT NULL,
 	RG			varchar(10),
 	DIR			varchar(1),
 	SUPPRESS	char(1) NOT NULL,
@@ -173,15 +174,15 @@ CREATE TABLE MRREL (
 
 
 CREATE TABLE MRSAB (
-	VCUI		char(12),
-	RCUI		char(12),
-	VSAB		varchar(50) NOT NULL,
-	RSAB		varchar(20) NOT NULL,
+	VCUI		char(8),
+	RCUI		char(8),
+	VSAB		varchar(40) NOT NULL,
+	RSAB		varchar(40) NOT NULL,
 	SON			text NOT NULL,
-	SF			varchar(20) NOT NULL,
-	SVER		varchar(20),
-	VSTART		char(10),
-	VEND		char(10),
+	SF			varchar(40) NOT NULL,
+	SVER		varchar(40),
+	VSTART		char(8),
+	VEND		char(8),
 	IMETA		varchar(10) NOT NULL,
 	RMETA		varchar(10),
 	SLC			text,
@@ -190,10 +191,10 @@ CREATE TABLE MRSAB (
 	TFR			int,
 	CFR			int,
 	CXTY		varchar(50),
-	TTYL		varchar(200),
+	TTYL		varchar(400),
 	ATNL		text,
 	LAT			char(3),
-	CENC		varchar(20) NOT NULL,
+	CENC		varchar(40) NOT NULL,
 	CURVER		char(1) NOT NULL,
 	SABIN		char(1) NOT NULL,
 	SSN			text NOT NULL,
@@ -203,16 +204,16 @@ CREATE TABLE MRSAB (
 
 
 CREATE TABLE MRSAT (
-	CUI			char(12) NOT NULL,
-	LUI			char(12),
-	SUI			char(12),
-	METAUI		varchar(50),
+	CUI			char(8) NOT NULL,
+	LUI			char(10),
+	SUI			char(10),
+	METAUI		varchar(100),
 	STYPE		varchar(50) NOT NULL,
-	CODE		varchar(50),
-	ATUI		varchar(15) NOT NULL,
+	CODE		varchar(100),
+	ATUI		varchar(11) NOT NULL,
 	SATUI		varchar(50),
-	ATN			varchar(80) NOT NULL,
-	SAB			varchar(20) NOT NULL,
+	ATN			varchar(100) NOT NULL,
+	SAB			varchar(40) NOT NULL,
 	ATV			text,
 	SUPPRESS	char(1) NOT NULL,
 	CVF			int,
@@ -221,8 +222,8 @@ CREATE TABLE MRSAT (
 
 
 CREATE TABLE MRSMAP (
-	MAPSETCUI	char(12),
-	MAPSETSAB	varchar(20),
+	MAPSETCUI	char(8),
+	MAPSETSAB	varchar(40),
 	MAPID		varchar(50),
 	MAPSID		varchar(50),
 	FROMEXPR	text,
@@ -237,11 +238,11 @@ CREATE TABLE MRSMAP (
 
 
 CREATE TABLE MRSTY (
-	CUI			char(12) NOT NULL,
+	CUI			char(8) NOT NULL,
 	TUI			char(4) NOT NULL,
 	STN			varchar(100) NOT NULL,
 	STY			varchar(50) NOT NULL,
-	ATUI		varchar(15) NOT NULL,
+	ATUI		varchar(11) NOT NULL,
 	CVF			int,
 	dummy 		char(1)
 );
@@ -250,67 +251,67 @@ CREATE TABLE MRSTY (
 CREATE TABLE MRXNS_ENG (
 	LAT			char(3) NOT NULL,
 	NSTR		text NOT NULL,
-	CUI			char(12) NOT NULL,
-	LUI			char(12) NOT NULL,
-	SUI			char(12) NOT NULL,
+	CUI			char(8) NOT NULL,
+	LUI			char(10) NOT NULL,
+	SUI			char(10) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE MRXNW_ENG (
 	LAT			char(3) NOT NULL,
-	NWD			varchar(100) NOT NULL,
-	CUI			char(12) NOT NULL,
-	LUI			char(12) NOT NULL,
-	SUI			char(12) NOT NULL,
+	NWD			varchar(200) NOT NULL,
+	CUI			char(8) NOT NULL,
+	LUI			char(10) NOT NULL,
+	SUI			char(10) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE MRAUI (
 	AUI1		varchar(9) NOT NULL,
-	CUI1		char(12) NOT NULL,
+	CUI1		char(8) NOT NULL,
 	VER			varchar(10) NOT NULL,
 	REL			varchar(4),
 	RELA		varchar(100),
 	MAPREASON	text NOT NULL,
 	AUI2		varchar(9) NOT NULL,
-	CUI2		char(12) NOT NULL,
+	CUI2		char(8) NOT NULL,
 	MAPIN		char(1) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE AMBIGSUI (
-	SUI			char(12) NOT NULL,
-	CUI			char(12) NOT NULL,
+	SUI			char(10) NOT NULL,
+	CUI			char(8) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE AMBIGLUI (
-	LUI			char(12) NOT NULL,
-	CUI			char(12) NOT NULL,
+	LUI			char(10) NOT NULL,
+	CUI			char(8) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE DELETEDCUI (
-	PCUI		char(12) NOT NULL,
+	PCUI		char(8) NOT NULL,
 	PSTR		text NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE DELETEDLUI (
-	PLUI		char(12) NOT NULL,
+	PLUI		char(10) NOT NULL,
 	PSTR		text NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE DELETEDSUI (
-	PSUI		char(12) NOT NULL,
+	PSUI		char(10) NOT NULL,
 	LAT			char(3) NOT NULL,
 	PSTR		text NOT NULL,
 	dummy 		char(1)
@@ -318,15 +319,15 @@ CREATE TABLE DELETEDSUI (
 
 
 CREATE TABLE MERGEDCUI (
-	PCUI		char(12) NOT NULL,
-	CUI			char(12) NOT NULL,
+	PCUI		char(8) NOT NULL,
+	CUI			char(8) NOT NULL,
 	dummy 		char(1)
 );
 
 
 CREATE TABLE MERGEDLUI (
-	PLUI		char(12),
-	LUI			char(12),
+	PLUI		char(10),
+	LUI			char(10),
 	dummy 		char(1)
 );
 
