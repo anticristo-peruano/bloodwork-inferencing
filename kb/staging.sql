23c56b680ac46a36c2be81d49be2734ae5a948d0
sofos <isoschronos@gmail.com>
2025-08-27 13:01:12 -0500

    Finished overviewing LOINC.

--- kb/staging.sql
+++ kb/staging.sql
@@ -1,10 +1,27 @@
 CREATE SCHEMA IF NOT EXISTS kg AUTHORIZATION postgres;
 SET search_path TO kg, loinc, snomedct, umls;
 
--- Hematology tests from LOINCS
-CREATE MATERIALIZED VIEW loinctest AS
+---- LOINC INFORMATION
+-- Hematology tests (lab observations)
+DROP MATERIALIZED VIEW IF EXISTS kg.loinc_test CASCADE;
+CREATE MATERIALIZED VIEW kg.loinc_test AS
 SELECT loinc_num, long_common_name, component, property, time_aspct, system,
        scale_typ, method_typ, class, classtype, example_ucum_units
 FROM loinc.loinc
 WHERE classtype = '1'
 AND (class ILIKE 'HEM%' OR class ILIKE 'COAG%' OR long_common_name ILIKE '%hematolog%');
+
+--SELECT * FROM kg.loinc_test LIMIT 20;
+
+-- Hematology analyte component
+DROP MATERIALIZED VIEW IF EXISTS kg.loinc_analyte CASCADE;
+CREATE MATERIALIZED VIEW kg.loinc_analyte AS
+SELECT t.loinc_num, p.partnumber, p.partname
+FROM kg.loinc_test t
+JOIN loinc.partlink_primary plp on t.loinc_num = plp.loinc_num
+JOIN loinc.partlink_supplementary pls on t.loinc_num = pls.loinc_num
+JOIN loinc.part p on p.partname = pls.partname
+WHERE pls.linktypename = 'DetailedModel' AND pls.parttypename = 'COMPONENT';
+
+--SELECT * FROM kg.loinc_analyte LIMIT 20;
+
--- kb/tests/loinc.sql
+++ kb/tests/loinc.sql
@@ -8,3 +8,12 @@ SELECT classtype, class, COUNT(*) as count
 FROM loinc.loinc
 GROUP BY classtype, class
 ORDER BY classtype, count DESC;
+
+-- Parts, primary and supplementary
+SELECT *
+FROM loinc.part p
+JOIN loinc.partlink_primary plp on p.partname = plp.partname
+JOIN loinc.partlink_supplementary pls on p.partname = pls.partname
+WHERE pls.linktypename = 'DetailedModel'
+  AND pls.parttypename = 'COMPONENT'
+LIMIT 20;
